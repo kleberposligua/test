f349e06d6ee08d3fbfaf153bf32d19d32a547b7c
kleber.posligua <kleberposligua@hotmail.com>
2016-05-19 16:26:45 -0500

    modificando saludo

--- pruebaGit/pruebaGit/frmHola.vb
+++ pruebaGit/pruebaGit/frmHola.vb
@@ -2,7 +2,7 @@
 
     Private Function saludos() As String
         Dim cad As String = "Hola"
-
+        cad = cad & "Probando git"
         Return cad
     End Function
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
