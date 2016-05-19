ad3c7187cbb95408f716b90ae238ef6bb0a91010
kleber.posligua <kleberposligua@hotmail.com>
2016-05-19 16:14:03 -0500

    Hola commit

---
+++ pruebaGit/pruebaGit/frmHola.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmHola
+
+    Private Function saludos() As String
+        Dim cad As String = "Hola"
+
+        Return cad
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(saludos)
+    End Sub
+End Class
